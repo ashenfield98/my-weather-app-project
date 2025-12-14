3abe73e7aa6d30f6d43bff4523834e00f9300084
ashenfield98 <ashley.enfield98@gmail.com>
2025-12-14 16:31:28 +0100

    added API in the search engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function changeWeatherDetails(response) {
+  let temperatureElement = document.querySelector("#weather-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "7b8fb87030c4beo42b4207t4f1d8b27a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(changeWeatherDetails);
+}
+
 function changeCityName(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCityName);
+searchCity("Amsterdam");
